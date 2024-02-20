8c3b9d7a8c77e2d4d68df9c708bc8728477c428b
RafaelContact <contactrodrigues21@gmail.com>
2024-02-20 14:48:47 -0300

    feat: create a function for repositories, input keyup 13, profile user

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,23 @@ document.getElementById('btn-search').addEventListener('click', () =>{
 
 })
 
+document.getElementById('input-search').addEventListener('keyup', (e) =>{
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        getUserProfile(userName)
+    }
+})
+
 async function user(userName){
     const response = await fetch(`https://api.github.com/users/${userName}`)
+    return await response.json()
+}
+
+async function repos(userName){
+    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
     return await response.json()
 }
 
@@ -14,7 +29,6 @@ async function user(userName){
 
 function getUserProfile(userName){
     user(userName).then((userData) => {
-
         // será preciso o avatar, o name e a bio.
         let userInfo = `<img src="${userData.avatar_url}" alt="foto do perfil" />
         <div class="data">
@@ -25,3 +39,15 @@ function getUserProfile(userName){
         document.querySelector('.profile-data').innerHTML = userInfo
     })
 }
+
+function getUserRepositories(userName){
+    repos(userName).then(reposData => {
+        let repositoriesItems = ""
+            reposData.forEach(repo => {
+                    repositoriesItems += `<li><a href="${repo.html_url}">${repo.name}</a></li>`
+                })
+
+                console.log(repositoriesItems)
+            })
+}
+getUserRepositories('rafinha-dev')
