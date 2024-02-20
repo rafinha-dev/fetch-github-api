83e66e940b34114e504c6ea8cdba93ef51f09ab2
RafaelContact <contactrodrigues21@gmail.com>
2024-02-20 16:53:39 -0300

    refactor: async await in get user promisse

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,29 +18,27 @@ document.getElementById('input-search').addEventListener('keyup', (e) =>{
         getUserProfile(userName)
     }
 })
-
-
-
-
 // console.log( await user())
 // await tirou o estado de pending
 
-function getUserProfile(userName){
-    getUser(userName).then((userData) => {
-        // será preciso o avatar, o name e a bio.
-        let userInfo = `
-        <div class="info">
-            <img src="${userData.avatar_url}" alt="foto do perfil" />
-            <div class="data">
-                <h1>${userData.name ?? 'Não possui nome cadastrado '}</h1>
-                <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
-            </div>
-        </div>`
+async function getUserProfile(userName){
+    const userResponse = await getUser(userName)
+        
+    
+    // getUser(userName).then((userData) => {
+    //     let userInfo = `
+    //     <div class="info">
+    //         <img src="${userData.avatar_url}" alt="foto do perfil" />
+    //         <div class="data">
+    //             <h1>${userData.name ?? 'Não possui nome cadastrado '}</h1>
+    //             <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
+    //         </div>
+    //     </div>`
 
-        document.querySelector('.profile-data').innerHTML = userInfo
+    //     document.querySelector('.profile-data').innerHTML = userInfo
 
-        getUserRepositories(userName)
-    })
+    //     getUserRepositories(userName)
+    // })
 }
 
 function getUserRepositories(userName){
