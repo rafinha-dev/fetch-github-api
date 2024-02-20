96a0f1f1ba81b72aed167c191f3548bff9b0ba34
RafaelContact <contactrodrigues21@gmail.com>
2024-02-19 23:12:18 -0300

    feat: tamplate string to search

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,23 @@
-async function user(){
-    const response = await fetch('https://api.github.com/users/rafinha-dev')
+async function user(userNeme){
+    const response = await fetch(`https://api.github.com/users/${userNeme}`)
     return await response.json()
 }
 
 // console.log( await user())
 // await tirou o estado de pending
 
-function getUserProfile(){
-    user().then((userData) => {
+function getUserProfile(userName){
+    user(userName).then((userData) => {
 
         // será preciso o avatar, o name e a bio.
         let userInfo = `<img src="${userData.avatar_url}" alt="foto do perfil" />
         <div class="data">
-            <h1>${userData.name ?? 'Não possui nome cadastrado  '}</h1>
-            <p>${userData.bio}</p>
+            <h1>${userData.name ?? 'Não possui nome cadastrado '}</h1>
+            <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
         </div>`
+
+        document.querySelector('.profile-data').innerHTML = userInfo
     })
 }
 
-getUserProfile()
+getUserProfile('dev-rafinha')
